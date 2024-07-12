bf009db05168cebb4f2318f40071b233040ea912
sudo-sango <66486318+sudo-sango@users.noreply.github.com>
2024-07-12 17:16:44 +0200

    Update Download_Comment.py

--- pages/Download_Comment.py
+++ pages/Download_Comment.py
@@ -90,7 +90,7 @@ st.markdown(
 
 
 ######################## ################################################################################################################################################################################
-
+api_key=api_key
 # Fonction pour télécharger les commentaires 
 def download_comments(video_id, api_key):     
     youtube = build('youtube', 'v3', developerKey=api_key)      
@@ -142,8 +142,7 @@ def download_comments(video_id, api_key):
 
 
 # Fonction pour afficher les informations de la vidéo 
-def display_video_info(video_id, api_key):
-    api_key = api_key
+def display_video_info(video_id, api_key):     
     youtube = build('youtube', 'v3', developerKey=api_key)     
     video = youtube.videos().list(part="snippet,statistics", id=video_id).execute()      
     if not video['items']:         
@@ -161,38 +160,6 @@ def display_video_info(video_id, api_key):
 
 
 ######################## ################################################################################################################################################################################
-
-# Fonction pour filtrer les commentaires
-def predictionS(df_comments):
-    df_comments['Likes'] = pd.to_numeric(df_comments['Likes'], errors='coerce')
-    df_comments['Reply Count'] = pd.to_numeric(df_comments['Reply Count'], errors='coerce')
-    df_comments['Likes'] = df_comments['Likes'].fillna(0).astype(int)
-    df_comments['Reply Count'] = df_comments['Reply Count'].fillna(0).astype(int)
-  
-    df_filtered = df_comments[(df_comments['Likes'] > 5) & (df_comments['Reply Count'] > 5)]
-    return df_filtered
-
-
-
-def predictionaa(df_comments):
-    try:
-        df_comments['Likes'] = df_comments['Likes'].fillna(0)
-        df_comments['Reply Count'] = df_comments['Reply Count'].fillna(0)
-        
-        df_comments['Likes'] = pd.to_numeric(df_comments['Likes'], errors='coerce').astype(int)
-        df_comments['Reply Count'] = pd.to_numeric(df_comments['Reply Count'], errors='coerce').astype(int)
-    except Exception as e:
-        st.error(f"Erreur lors du remplissage des valeurs NaN et de la conversion en entier : {e}")
-        return pd.DataFrame()  # Retourne un DataFrame vide en cas d'erreur
-
-    try:
-        df_filtered = df_comments[(df_comments['Likes'] > 5) & (df_comments['Reply Count'] > 5)]
-    except Exception as e:
-        st.error(f"Erreur lors du filtrage du DataFrame : {e}")
-        return pd.DataFrame()  # Retourne un DataFrame vide en cas d'erreur
-
-    return df_filtered
-    #le script renvois cette erreur corrige <Erreur lors du remplissage des valeurs NaN et de la conversion en entier : Cannot convert non-finite values (NA or inf) to integer>
 
 
 def prediction(df_comments):
@@ -215,38 +182,8 @@ def prediction(df_comments):
 
     return df_filtered
 
-#ooptimise moi cette fonction car la premiere chose qu'elle doit faire c'est de supprimer la premiere ligne de df qui est une ligne qui contient [Name,Likes etcc]
 
 
-def predictionaaaaa(df_comments):
-    try:
-        df_comments['Likes'] = pd.to_numeric(df_comments['Likes'], errors='coerce')
-        
-        df_comments['Reply Count'] = pd.to_numeric(df_comments['Reply Count'], errors='coerce')
-    except Exception as e:
-        st.error(f"Erreur lors de la conversion de 'Likes' en numérique : {e}")
-        return pd.DataFrame()  # Retourne un DataFrame vide en cas d'erreur
-
-    #try:
-    #    df_comments['Reply Count'] = pd.to_numeric(df_comments['Reply Count'], errors='coerce')
-    #except Exception as e:
-    #    st.error(f"Erreur lors de la conversion de 'Reply Count' en numérique : {e}")
-    #    return pd.DataFrame()  # Retourne un DataFrame vide en cas d'erreur
-
-    try:
-        df_comments['Likes'] = df_comments['Likes'].fillna(0).astype(int)
-        df_comments['Reply Count'] = df_comments['Reply Count'].fillna(0).astype(int)
-    except Exception as e:
-        st.error(f"Erreur lors du remplissage des valeurs NaN et de la conversion en entier : {e}")
-        return pd.DataFrame()  # Retourne un DataFrame vide en cas d'erreur
-
-    try:
-        df_filtered = df_comments[(df_comments['Likes'] > 5) & (df_comments['Reply Count'] > 5)]
-    except Exception as e:
-        st.error(f"Erreur lors du filtrage du DataFrame : {e}")
-        return pd.DataFrame()  # Retourne un DataFrame vide en cas d'erreur
-
-    return df_filtered
 
 ######################## ################################################################################################################################################################################
 
@@ -257,10 +194,6 @@ def extract_video_id(link):
     return video_id
 ######################## ################################################################################################################################################################################
 
-def create_folder(folder_name):
-  if not os.path.exists(folder_name):
-    os.makedirs(folder_name)
-######################## ################################################################################################################################################################################
 
 
 def main():
@@ -274,7 +207,9 @@ def main():
     
     st.markdown(html_titre, unsafe_allow_html = True)
 
+   
     st.markdown('<p style="text-align: center;font-size:15px;" > <br><br><br><bold><center><h1 style="color:#D3F7F4"> <bold>ENTREZ LE LIEN DE LA VIDEO DONT VOUS VOULEZ TELECHARGER LES COMMENTAIRES<h1></bold><p>', unsafe_allow_html=True)
+    
     
     video_url = st.text_input('Entrez le lien de la vidéo Youtube :')
 
@@ -305,19 +240,9 @@ def main():
             df_comments = download_comments(video_id, api_key)
             video_title = display_video_info(video_id, api_key)
 
-            # Créez les dossiers
-            create_folder('a')
-            create_folder('b')
-
-            # Enregistrez les commentaires dans le dossier "a"
-            comments_path = os.path.join('a', f"{video_title}.xlsx")
-            df_comments.to_excel(comments_path, index=False)
-
+    
             df_filtered = prediction(df_comments)
 
-            # Enregistrez le fichier de prédiction dans le dossier "b"
-            predict_path = os.path.join('b', f"{video_title}_PREDICTION.xlsx")
-            df_filtered.to_excel(predict_path, index=False)
 
             # Préparation des fichiers pour téléchargement
             output_comments = io.BytesIO()
@@ -334,11 +259,9 @@ def main():
             output_prediction.seek(0)
             excel_file_prediction = output_prediction.getvalue()
 
+           
             st.markdown(f'<a href="data:application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;base64,{base64.b64encode(excel_file_comments).decode("utf-8")}" download="{video_title}.xlsx">Télécharger les commentaires</a>', unsafe_allow_html=True)
             st.markdown(f'<a href="data:application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;base64,{base64.b64encode(excel_file_prediction).decode("utf-8")}" download="{video_title}_PREDICTION.xlsx">Télécharger le fichier de commentaires predictif</a>', unsafe_allow_html=True)
-           
-
-           
             # Barre de progression
             progress_text = "Operation in progress. Please wait."
             my_bar = st.progress(0, text=progress_text)
@@ -346,14 +269,6 @@ def main():
             for percent_complete in range(100):
                 time.sleep(0.1)
                 my_bar.progress(percent_complete + 1, text=progress_text)
-
-
-
-
-
-  
-
-
 
 if __name__ == "__main__":
     main()
